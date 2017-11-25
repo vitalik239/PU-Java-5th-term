6f9b7a6c293a9ea1107e2d6b328aa25e479add6e
vitalik <vitalik239>
2017-11-25 12:07:33 +0300

    minor

--- src/main/java/lab/functions/Function.java
+++ src/main/java/lab/functions/Function.java
@@ -7,6 +7,7 @@ import lab.Logger;
 import lab.exceptions.WrongClassFunctionResult;
 
 import java.io.InputStream;
+import java.util.Set;
 
 public abstract class Function<S, T> {
     protected final S previous_result;
@@ -22,9 +23,10 @@ public abstract class Function<S, T> {
             this.config = null;
         }
         previous_result = input;
+        Logger.log(this.toString() + "created");
     }
 
-    public Function(String configPath, Function<?, S> previous) throws Exception {
+    public Function(String configPath, Function previous) throws Exception {
         if (!configPath.isEmpty()) {
             InputStream configStream = FileUtil.getInputStream(configPath);
             this.config = new FunctionConfig(configStream);
@@ -33,22 +35,33 @@ public abstract class Function<S, T> {
             this.config = null;
         }
 
-        if (previous.resultClass() != this.inputClass()) {
-            throw new WrongClassFunctionResult("Expected smth else");
+        if (previous.resultClass().contains(this.inputClass())) {
+            previous_result = previous.getResult(this.inputClass());
         } else {
-            previous_result = previous.getResult();
+            throw new WrongClassFunctionResult("Expected smth else");
         }
+        Logger.log(this.toString() + "created");
+    }
+
+    public static abstract class inputConverter {
+        abstract T convert
+    }
+
+    public static abstract class outputConverter {
+        abstract Object convert(Class cls);
     }
 
     public abstract void execute() throws Exception;
 
-    public abstract Class inputClass();
+    public abstract Set<Class> inputClasses();
 
-    public Class resultClass() {
-        return result.getClass();
+    public abstract Set<Class> outputClasses();
+
+    public Object getResult(Class clz) {
+        return outputConverter.convert(clz);
     }
 
-    public T getResult() {
+    public T getStandartResult() {
         return result;
     }
 }
